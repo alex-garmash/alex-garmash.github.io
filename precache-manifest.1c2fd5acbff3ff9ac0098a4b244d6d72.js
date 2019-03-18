8c40a1073aab23b59b63b161ae759ca91184a2a9
alex-garmash <44101733+alex-garmash@users.noreply.github.com>
2019-03-18 16:13:35 +0200

    :)
    new

---
+++ precache-manifest.1c2fd5acbff3ff9ac0098a4b244d6d72.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "073d8bd7febb828da6db",
+    "url": "/static/js/main.ca8de357.chunk.js"
+  },
+  {
+    "revision": "26ad5a2044e18dc944ce",
+    "url": "/static/js/2.fb4aa602.chunk.js"
+  },
+  {
+    "revision": "92fc53ac93b0aa7997c927c07943db5b",
+    "url": "/index.html"
+  }
+];
