8d76f64d363c25107acc851526f70b8a2ff05fa5
alex-garmash <44101733+alex-garmash@users.noreply.github.com>
2019-03-18 16:17:58 +0200

    Demo

---
+++ precache-manifest.9a6e60cc1b17992bd89039c70e107eee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "77f2dc2988f6b2e641cc",
+    "url": "/static/js/main.dd9d03e3.chunk.js"
+  },
+  {
+    "revision": "3d0dc74cba30b072de0a",
+    "url": "/static/js/2.f4596b23.chunk.js"
+  },
+  {
+    "revision": "507bbf359fd504fa1b709aa77650d370",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1c2fd5acbff3ff9ac0098a4b244d6d72.js"
+  "/precache-manifest.9a6e60cc1b17992bd89039c70e107eee.js"
 );
 
 workbox.clientsClaim();
